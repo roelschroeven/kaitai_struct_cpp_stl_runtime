9c089323764ca80e13b3ae0fd1f5f7e5405d927e
Mikhail Yakshin <greycat@altlinux.org>
2023-06-16 19:17:11 +0100

    Fix MinGW DLL copying + add searching for test executable in 2 different places in run-unittest.ps1

--- .build/build-mingw.ps1
+++ .build/build-mingw.ps1
@@ -1,9 +1,9 @@
 <#
 .DESCRIPTION
-Builds Kaitai Struct C++ runtime library and unit tests on Windows using Mingw
+Builds Kaitai Struct C++ runtime library and unit tests on Windows using MinGW
 
 Requires:
-- Mingw installed and available in the command prompt
+- MinGW installed and available in the command prompt
 - cmake/ctest available
 - GTest installed, path passed in `-GTestPath`
 #>
@@ -29,6 +29,16 @@ cd build
 cmake -DCMAKE_PREFIX_PATH="$GTestPath" -DSTRING_ENCODING_TYPE=NONE -G "MinGW Makefiles" ..
 cmake --build . --config Debug
 
-Get-ChildItem -Recurse
+# MinGW Makefiles build generates the following output:
+#
+# - build/libkaitai_struct_cpp_stl_runtime.dll
+# - build/libkaitai_struct_cpp_stl_runtime.dll.a
+# - build/tests/unittest.exe
+#
+# unittest.exe links dynamically against GTest DLLs and `libkaitai_struct_cpp_stl_runtime.dll`, and it
+# will need all of them in same directory as .exe to run it, so we copy it.
+
+cp $GTestPath\debug\bin\*.dll tests
+cp libkaitai_struct_cpp_stl_runtime.dll tests\unittest.exe
 
 Pop-Location
--- .build/run-unittest.ps1
+++ .build/run-unittest.ps1
@@ -18,6 +18,13 @@ cd build
 #ctest -C Debug --output-on-failure
 
 # Run gtest-generated binary directly, produces more detailed output
-./tests/Debug/unittest.exe
+if (Test-Path './tests/Debug/unittest.exe') {
+    ./tests/Debug/unittest.exe
+} elseif (Test-Path './tests/unittest.exe') {
+    ./tests/unittest.exe
+} else {
+    Write-Error "Unable to find the unittest executable."
+    exit 1
+}
 
 Pop-Location
