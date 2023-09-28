1a3a87e720c2497e8dd9d70ba918f0e7d60c6f80
Roel Schroeven <roel@tresco.eu>
2023-09-28 13:37:05 +0200

    Batch-file voor 'release' naar C:\Libs

---
+++ C++Builder_Berlin/release_to_libs.bat
@@ -0,0 +1,13 @@
+@echo off
+
+set TARGET_DIR=C:\Libs\kaitai_struct_cpp_stl_runtime
+
+mkdir %TARGET_DIR% 2> NUL
+mkdir %TARGET_DIR%\include 2> NUL
+mkdir %TARGET_DIR%\include\kaitai 2> NUL
+copy ..\kaitai\kaitaistream.h %TARGET_DIR%\include\kaitai
+copy ..\kaitai\kaitaistruct.h %TARGET_DIR%\include\kaitai
+copy ..\kaitai\exceptions.h %TARGET_DIR%\include\kaitai
+
+mkdir "%TARGET_DIR%\Lib_C++Builder_Berlin" 2> NUL
+copy Lib\kaitai_struct_cpp_stl_runtime.lib "%TARGET_DIR%\Lib_C++Builder_Berlin"
